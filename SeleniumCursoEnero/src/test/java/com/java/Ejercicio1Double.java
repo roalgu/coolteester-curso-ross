3ef5f4836309691c450dc3d785ab6785145ac934
Rosario <ross.enigma@gmail.com>
2022-01-30 20:45:44 -0600

    Tarea 1 30 de enero 2022

---
+++ SeleniumCursoEnero/src/test/java/com/java/Ejercicio1Double.java
@@ -0,0 +1,28 @@
+package com.java;
+
+public class Ejercicio1Double {
+
+	public static void main(String[] args) {
+		// Ejercicio 1
+		//Crear una clase (public static void main) con tres variables
+		//de tipo double e imprimir la suma, resta, multiplicación y división de las 3 variables. 
+		
+		double a = 1.1;
+		double b = 2.2;
+		double c = 3.1;
+		
+		double sum1 = a+b+c;
+		double rest2 = a-b-c;
+		double mult3 = a* b*c;
+		double div4 = (a+b)/c;
+
+		System.out.println("El valor double de la suma es   "+  sum1);
+		System.out.println("El valor double de la resta es  "+  rest2);
+		System.out.println("El valor double de la multiplicación es  "+  mult3);
+		System.out.println("El valor doble de la division es   "+ div4);
+		
+		
+		
+	}
+
+}
---
+++ SeleniumCursoEnero/src/test/java/com/java/Ejercicio2String.java
@@ -0,0 +1,19 @@
+package com.java;
+
+public class Ejercicio2String {
+
+	public static void main(String[] args) {
+		// Crear una clase (public static void main) con 4 variables tipo String con los siguientes valores: 
+		
+		String var1 = "Cool";
+		String var2 = "Selenium con Java ";
+		String var3 = "Curso de ";
+		String var4 = "Testers ";
+		
+		System.out.println(var3 + var2 + var1 +  var4);
+		
+		
+	
+	}
+
+}
