4177c50a405b54ea184a710f59c81cbfdacc8bcb
Rosario <ross.enigma@gmail.com>
2022-02-01 10:29:54 -0600

    Operadores logicos

---
+++ SeleniumCursoEnero/src/test/java/com/java/Ejercicio4OperadoresLogicos.java
@@ -0,0 +1,28 @@
+package com.java;
+
+public class Ejercicio4OperadoresLogicos {
+
+	public static void main(String[] args) {
+		// Declara 2 variables numéricas enteras (con el valor que desees), 
+//		he indica cual es mayor de los dos. Si son iguales indicarlo también.
+//		Ves cambiando los valores para comprobar que funciona. 
+		
+		int or = 10;
+		int or2 = 8;
+		
+		if(or > or2) {
+			System.out.println("ES MAYOR");
+		}
+		else if (or < or2){
+			System.out.println("ES MENOR");
+		}
+		else if (or == or2){
+			System.out.println("ES IGUAL");
+		}
+		}
+		
+	}
+
+
+
+
