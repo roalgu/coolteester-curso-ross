72cc235805441b7cc648fe7f34531e99b6e3b867
Rosario <ross.enigma@gmail.com>
2022-02-12 11:08:18 -0600

    first test

--- SeleniumCursoEnero/src/test/java/com/java/ShouldWakeUp.java
+++ SeleniumCursoEnero/src/test/java/com/java/ShouldWakeUp.java
@@ -5,6 +5,7 @@ public class ShouldWakeUp {
 	public static void main(String[] args) {
 		boolean barking = true;
 		int hourOfDay = 8;
+		
 		// the dog is barking before 8 or after 22 hours so in that case return true
 		if (barking == true && hourOfDay >= 0 && hourOfDay <= 23) {
 			if (barking == true && hourOfDay <= 8 && hourOfDay >= 22) {
---
+++ SeleniumCursoEnero/src/test/java/com/java/VerificadorNumeros.java
@@ -0,0 +1,23 @@
+package com.java;
+
+public class VerificadorNumeros {
+	public static void main(String[] args) {
+		imprimirVerificadorNumeros();
+}
+
+	public static void imprimirVerificadorNumeros() {
+		 int numero1 = 1;
+		 int numero2 = 2;
+		// int numero3 = 1;
+		 
+		 if (numero1==numero2);{
+		 	System.out.println ("Los numeros son iguales");
+		 } else if {
+			 System.out.println("Los numeros son diferentes");
+		 }
+			}
+}
+
+
+
+
