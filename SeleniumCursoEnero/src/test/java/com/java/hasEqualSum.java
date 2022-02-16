21f390663d0d495d2ec0aef2f46004d9ecaada11
Rosario <ross.enigma@gmail.com>
2022-02-15 18:11:10 -0600

    borre una clase

---
+++ SeleniumCursoEnero/src/test/java/com/java/hasEqualSum.java
@@ -0,0 +1,23 @@
+package com.java;
+
+public class hasEqualSum {
+
+	public static void main(String[] args) {
+		boolean val = true;
+		int val1 = 4;
+		int val2 = 4;
+		int val3 = 9;
+		
+		double sum = val1+ val2;
+		System.out.println("La suma es de:"+""+sum);
+		
+	if (val==true && sum ==val3) {
+		System.out.println("true");
+	} else {
+		System.out.println("False");
+	}
+		}
+
+	}
+
+
--- SeleniumCursoEnero/src/test/java/com/java/printMegaBytesAndKiloBytes.java
+++ SeleniumCursoEnero/src/test/java/com/java/printMegaBytesAndKiloBytes.java
@@ -5,14 +5,20 @@ public class printMegaBytesAndKiloBytes {
 	public static void main(String[] args) {
 		// 1 MB = 1024 KB
 		int kiloBytes = 2500;
+		
+		if (kiloBytes <= 0) {
+			System.out.println("Valor invalido");
+		} else {
 
-		double mult = 2500 * 1;
-		double div = mult / 1024;
+			double mult = kiloBytes * 1;
+			double div = mult / 1024;
 
-		double mult2 = div * 1024;
-		double div2 = mult2 / 1;
+			double mult2 = div * 1024;
+			double div2 = mult2 / 1;
 
-		System.out.println(kiloBytes + "" + "KB" + " " + String.format("%.0f", div) + "" + "MB" + " "+ String.format("%.0f", div2) + "" + "KB");
+			System.out.println(kiloBytes + "" + "KB" + " " + String.format("%.0f", div) + "" + "MB" + " "
+					+ String.format("%.0f", div2) + "" + "KB");
+		}
 	}
 
 }
